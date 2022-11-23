c16ac1186df0c6d457e9f4e9e11ed70ac335fa11
Dylan Prosser <dprosser4@gmail.com>
2022-11-23 21:11:06 +0000

    completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,15 @@
+// create your loops here.
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(i * 2);
+}
+
+for (let i = 100; i >= 0; i--) {
+  console.log('Time till explosion: ' + i);
+  if (i === 0) {
+    console.log('Boom!');
+  }
+}
